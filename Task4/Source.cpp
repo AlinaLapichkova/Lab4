89c44496da37124c7d2a4aa68883aff31ff6fa1d
Computer <Computer@DESKTOP-VVFKOK5>
2021-11-25 19:42:52 +0300

    Добавьте файлы проекта.

---
+++ Task4/Source.cpp
@@ -0,0 +1,186 @@
+#include <Windows.h>
+#include <iostream>
+#include "stdio.h"
+#include <iomanip>
+#include <stdlib.h>
+using namespace std;
+
+int main()
+{
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+    double c = 0, n;
+    cout << " Введите количество членов последовательности(n > 0) n = ";
+    cin >> n;
+    bool firfirtest = true;
+    while (n <= 0 || n != int(n) || !firfirtest == cin.good())
+    {
+        cin.clear();
+        cin.ignore(255, '\n');
+        cout << "\n Введите количество членов последовательности (n > 0) n = ";
+        cin >> n;
+    }
+    while (c == 0) {
+        cout << "\n Выберите необходимое действие:\n 1. Задание 1\n 2. Задание 2\n 3. Завершить работу\n\n Введите выбранное число: ";
+        cin >> c;
+        bool firtest = true;
+        while (c < 1 || c > 3 || c != int(c) || !firtest == cin.good()) {
+            cin.clear();
+            cin.ignore(255, '\n');
+            cout << "\n Вы ввели некорректные данные.\n Введите целое число от 1 до 3 : ";
+            cin >> c;
+        }
+        if (c == 1) {
+            double x;
+            bool test = true;
+            do {
+                cout << " Введите x для нахождения частичной суммы ряда (x > 1) x = ";
+                cin >> x;
+                if (!test == cin.good() || x <= 1) {
+                    system("cls");
+                    cout << "\n\t Ошибка ввода (x > 1)\n ";
+                    cin.clear();
+                    cin.ignore(255, '\n');
+                }
+                else
+                    test = false;
+            } while (test);
+            double a, m, h;
+            double fact = 1;
+            a = -1;
+            m = 0;
+            h = 1;
+            double sum1 = 0;
+            double sum = 0;
+            int j;
+            for (int i = 1; i <= n; i++) {
+                for (j = 0; j < 2 * i; j++) { //факториал
+                    j++;
+                    sum1 = static_cast<double>(1) / j;
+                    fact *= sum1;
+                    j--;
+                }
+                a *= -1;
+                m = (x * x);
+                h *= m;
+                sum += (a * h * fact);
+                if (i != n) { //последний член ряда
+                    fact = 1;
+                }
+            }
+            double s;
+            s = 1 - sum;
+            cout << "\n Сумма ряда s = " << setprecision(11) << s << " \n";
+            double r;
+            r = cos(x) - s;
+            cout << " Абсолютная погрешность равна r = " << abs(r) << "\n";
+            double p = a * h * fact;
+            cout << " Абсолютная величина последнего слагаемого p = " << abs(p) << "\n\n";
+            if (abs(r) > abs(p)) {
+                cout << " Абсолютная погрешность больше абсолютной величины последнего слагаемого\n";
+            }
+            else if (abs(p) > abs(r)) {
+                cout << " Абсолютная величина последнего слагаемого больше абсолютной погрешности\n";
+            }
+            else cout << " Абсолютная погрешность равна абсолютной величине последнего слагаемого\n";
+            c = 0;
+        }
+        else if (c == 2) {
+
+            double x, E;
+            bool test = true;
+            do {
+                cout << "\n Задайте точность вычисления Е = ";
+                cin >> E;
+                cout << " \n Введите x для нахождения частичной суммы ряда (x > 1) x = ";
+                cin >> x;
+                if (!test == cin.good() || x <= 1) {
+                    system("cls");
+                    cout << "\n\t Ошибка ввода (x > 1; n > 0)\n";
+                    cin.clear();
+                    cin.ignore(255, '\n');
+                   
+                }
+                else
+                    test = false;
+            } while (test);
+            double a, m, h;
+            double fact = 1;
+            a = -1;
+            m = 0;
+            h = 1;
+            double sum1 = 0;
+            double summ = 0;
+            int j;
+            double sum = 0, sum2 = 0, k = 0, d = 0;
+            for (int i = 1; i <= n; i++) {
+                for (j = 0; j < 2 * i; j++) {
+                    j++;
+                    sum1 = static_cast<double>(1) / j;
+                    fact *= sum1;
+                    j--;
+                }
+                a *= -1;
+                m = (x * x);
+                h *= m;
+                summ = (a * h * fact);
+                if (i != n) {
+                    fact = 1;
+                }
+                if (abs(summ) > E) {
+                    sum += summ;
+                    k++;
+                }
+                if (abs(summ) > E * 10) {
+                    sum2 += summ;
+                    d++;
+                }
+            }
+            if ((sum != 0) && (sum2 != 0)) {
+                if (1 > E * 10) {
+                    sum = 1 - sum;
+                    sum2 = 1 - sum2;
+                    k++;
+                    d++;
+                }
+                cout << "\n Сумма элементов больших чем Е равна sum = " << sum << "\n Кол-во таких элементов k = " << k << "\n\n";
+                cout << " Сумма элементов больших чем Е*10 (Е увеличенно на порядок) равна sum2 = " << sum2 << "\n Кол-во таких элементов d = " << d << "\n\n";
+            }
+            else if ((sum != 0) && (sum2 == 0)) {
+                if (1 > E * 10) {
+                    sum = 1 - sum;
+                    sum2 = 1 - sum2;
+                    k++;
+                    d++;
+                    cout << "\n Сумма элементов больших чем Е равна sum = " << sum << "\n Кол-во таких элементов k = " << k << "\n\n";
+                    cout << " Сумма элементов больших чем Е*10 (Е увеличенно на порядок) равна sum2 = " << sum2 << "\n Кол-во таких элементов d = " << d << "\n\n";
+                }
+                else if (1 > E) {
+                    sum = 1 - sum;
+                    cout << "\n Сумма элементов больших чем Е равна sum = " << sum << "\n Кол-во таких элементов k = " << k << "\n\n";
+                    cout << " Элементов больших чем Е*10 (E увеличенно на порядок) не найдено\n\n";
+                }
+            }
+            else if ((sum == 0) && (sum2 == 0)) {
+                if (1 > E * 10) {
+                    sum = sum2 = 1;
+                    k = d = 1;
+                    cout << "\n Сумма элементов больших чем Е равна sum = " << sum << "\n Кол-во таких элементов k = " << k << "\n\n";
+                    cout << " Сумма элементов больших чем Е*10 (Е увеличенно на порядок) равна sum2 = " << sum2 << "\n Кол-во таких элементов d = " << d << "\n\n";
+                }
+                else if (1 > E) {
+                    sum = 1;
+                    k = 1;
+                    cout << "\n Сумма элементов больших чем Е равна sum = " << sum << "\n Кол-во таких элементов k = " << k << "\n\n";
+                    cout << " Элементов больших чем Е*10 (E увеличенно на порядок) не найдено\n\n";
+                }
+                else
+                    cout << "\n Элементов больших чем Е и Е*10 (увеличенного на порядок) не найдено\n\n";
+            }
+            c = 0;
+        }
+        else if (c == 3) {
+            return 0;
+        }
+    }
+}
